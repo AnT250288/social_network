0799a9cd6918b8ba505d4211fd60835b103b1ea0
AntonAl <_3arc_@tut.by>
2021-06-23 15:38:57 +0300

    12_reakt_kmb

---
+++ src/componens/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className={'header'}>
+            <img
+                src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdu0ziOCxst9tAmHGu7CrHSezLS9Jq7dKtSQ&usqp=CAU'}/>
+        </header>
+    );
+}
